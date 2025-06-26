54fbb6527faebc151572ec6fa9efabb9c83b80e2
Square485 <125174831+Square485@users.noreply.github.com>
2025-06-26 16:21:07 +0700

    Add files via upload

---
+++ php-guess-game-main/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>f095d13a</title>
+</head>
+<body>
+  <h1>Welcome to my guessing game</h1>
+
+  <?php
+$correctnumber=50; //change this number //
+if(isset($_GET['guess']))
+{
+  if (is_numeric($_GET['guess'])===FALSE)
+ {
+echo "Your guess is too short";
+}
+else if ($_GET['guess']<$correctnumber)
+ {
+echo "Your guess is too low";
+}
+else if ($_GET['guess']>$correctnumber)
+ {
+echo "Your guess is too high";
+}
+else if ($_GET['guess']==$correctnumber)
+ {
+echo "Congratulations - You are right";
+}
+}
+else
+{
+echo "Missing guess parameter <br> Your guess is too short";
+}
+?>
+</body>
+</html>
